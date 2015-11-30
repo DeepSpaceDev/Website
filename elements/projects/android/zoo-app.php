1b1304322684787ae8527678c9cb1d2e2cade9ad
Xeselaro <sese.tailor@gmail.com>
2015-11-30 23:31:46 +0100

    Add Google Play badge

--- elements/projects/android/zoo-app.php
+++ elements/projects/android/zoo-app.php
@@ -39,6 +39,7 @@
 					<div class="centerer"><img id="appicon" width="200px" src="../../../img/projects/android/zooapp_icon.png" /></div>
 					<span id="apptitle">
 						<h2>Zoo App</h2>
+						<a href="https://play.google.com/store/apps/details?id=de.sese7.zooapp&utm_source=global_co&utm_medium=prtnr&utm_content=Mar2515&utm_campaign=PartBadge&pcampaignid=MKT-AC-global-none-all-co-pr-py-PartBadges-Oct1515-1" target="_blank"><img width="150px" alt="Get it on Google Play" src="https://play.google.com/intl/en_us/badges/images/apps/en-play-badge.png" /></a>
 					</span>
 				</div>
 			</paper-material>
