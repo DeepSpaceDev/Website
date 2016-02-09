233d99453f94b18b1ac8045f4bba8ecd2f84b08b
Xeselaro <sese.tailor@gmail.com>
2016-02-09 22:15:24 +0100

    Bugfix with ?p and URL beautifier

--- index.php
+++ index.php
@@ -5,14 +5,14 @@ if(!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == ""){
     header("Location: $redirect");
 }
 $url = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-if(substr($url, -1) != '/'){
+if(substr($url, -1) != '/' && !strpos($url, '?')){
 	header("Location: $url/");
 }
 require_once("scripts/tools/UserAgendParser.php");
 //https://donatstudios.com/PHP-Parser-HTTP_USER_AGENT
 $browser = parse_user_agent()['browser'];
 $platform = parse_user_agent()['platform'];
-if(($browser == "MSIE" || $browser == "IEMobile") && $_GET["MSIE"] == true){
+if(($browser == "MSIE" || $browser == "IEMobile") && $_GET["msie"] != true){
 	header("Location: https://www.deepspace.onl/errors/unsupported_browser.html");
 	exit();
 }
@@ -34,7 +34,7 @@ if(($browser == "MSIE" || $browser == "IEMobile") && $_GET["MSIE"] == true){
 		<link rel="import" href="elements/elements.html">
 		<link rel="shortcut icon" href="img/favicon.png">
 		<link type="text/css" rel="stylesheet" href="css/style.css">
-		<?php if(isset($_GET["p"])){echo "<script>var isdialog = true; var dialogcontent = '" . $_GET["p"] . "';</script>";}?>	
+		<?php if(isset($_GET["p"])){echo "<script>var isdialog = true; var dialogcontent = '" . $_GET["p"] . "';</script>";}?>
 		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 		<script src="js/main.js"></script>
 
