a3fa1c3f701757a2de783de9cfb14aa0f3f7cb23
Xeselaro <sese.tailor@gmail.com>
2016-02-12 22:20:59 +0100

    Adding object size function

--- js/tools.js
+++ js/tools.js
@@ -131,3 +131,10 @@ String.prototype.hexDecode = function(){
 
     return back;
 }
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
