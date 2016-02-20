f7e6dbaec02bad5241614c2b9c2a0d732967d44f
Xeselaro <sese.tailor@gmx.net>
2016-02-20 13:37:19 +0100

    Update to btv v5.1.0

--- js/projects/btv.js
+++ js/projects/btv.js
@@ -1,3 +1,23 @@
+function getCookie(c_name) {
+    if (document.cookie.length > 0) {
+        c_start = document.cookie.indexOf(c_name + "=");
+        if (c_start != -1) {
+            c_start = c_start + c_name.length + 1;
+            c_end = document.cookie.indexOf(";", c_start);
+            if (c_end == -1) {
+                c_end = document.cookie.length;
+            }
+            return unescape(document.cookie.substring(c_start, c_end));
+        }
+    }
+    return "";
+}
+function setCookie(name, value, expiration){
+	var d = new Date();
+	d.setTime(d.getTime() + (expiration * 24 * 60 * 60 * 1000));
+
+	document.cookie = name + "=" + value + "; expires=" + d.toUTCString() + "; path=/";
+}
 function handDrawCircle(ctx, cx, cy, r, rounds, callback) {
 
     rounds = rounds ? rounds : 3.0; // can be fractional, ie. 2.5
